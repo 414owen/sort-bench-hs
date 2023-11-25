b811fc8ac9e758f3246728b99b97ec490685744a
Owen Shepherd <owen@owen.cafe>
2023-11-25 16:32:58 +0100

    feat: Successively more expensive projections

--- app/Main.hs
+++ app/Main.hs
@@ -15,47 +15,8 @@ import OtherSorts
 aList :: [Int]
 aList = take 10_000 $ cycle $ concat $ permutations [0..20]
 
-tupList :: [(Int, Int)]
-tupList = zip aList [0..]
-
--- * Large right-tuples
--- maybe cache lines play a role?
-
-type SevenTup a = ((), (), (), (), (), (), a)
-
-toSeventh :: a -> SevenTup a
-toSeventh a = ((), (), (), (), (), (), a)
-
-seventh :: SevenTup a -> a
-seventh (_, _, _, _, _, _, a) = a
-
-sevenTupList :: [SevenTup Int]
-sevenTupList = toSeventh <$> aList
-
-
--- * Nested tuples
-
-type LNestedTup a = (((((((a, ()), ()), ()), ()), ()), ()), ())
-
-nestedFst :: LNestedTup a -> a
-nestedFst = fst . fst . fst . fst . fst . fst . fst
-
-toNestedFst :: a -> LNestedTup a
-toNestedFst a = (((((((a, ()), ()), ()), ()), ()), ()), ())
-
-nestedLTupList :: [LNestedTup Int]
-nestedLTupList = toNestedFst <$> aList
-
-type RNestedTup a = ((), ((), ((), ((), ((), ((), ((), a)))))))
-
-nestedSnd :: RNestedTup a -> a
-nestedSnd = snd . snd . snd . snd . snd . snd . snd
-
-toNestedSnd :: a -> RNestedTup a
-toNestedSnd a = ((), ((), ((), ((), ((), ((), ((), a)))))))
-
-nestedRTupList :: [RNestedTup Int]
-nestedRTupList = toNestedSnd <$> aList
+embedFst :: a -> (a, ())
+embedFst a = (a, ())
 
 -- * Test tree
 
@@ -63,9 +24,9 @@ sorts :: Ord b => [(String, (a -> b) -> [a] -> [a])]
 sorts =
   [ ("sortOn", sortOn)
   , ("sortWith", sortWith)
-  , ("sortBy . comparing", sortWith')
-  , ("mergeSortWithVec", mergeSortWithVec)
-  , ("mergeSortWithList", mergeSortWithList)
+  -- , ("sortBy . comparing", sortWith')
+  -- , ("mergeSortWithVec", mergeSortWithVec)
+  -- , ("mergeSortWithList", mergeSortWithList)
   ]
 
 runSorts :: (NFData a, Ord b) => String -> (a -> b) -> [a] -> Benchmark
@@ -77,9 +38,23 @@ runSorts projString projection list
 main :: IO ()
 main = defaultMain
   [ runSorts "id" id aList
-  , runSorts "fst" fst tupList
-  , runSorts "snd" snd $ swap <$> tupList
-  , runSorts "seventh" seventh sevenTupList
-  , runSorts "(fst . fst . fst . fst . fst . fst . fst)" nestedFst nestedLTupList
-  , runSorts "(snd . snd . snd . snd . snd . snd . snd)" nestedSnd nestedRTupList
+  , runSorts "fst" fst $ embedFst <$> aList
+  , runSorts "fst . fst"
+      (fst . fst)
+    $ (embedFst . embedFst) <$> aList
+  , runSorts "fst . fst . fst"
+      (fst . fst . fst)
+    $ (embedFst . embedFst . embedFst) <$> aList
+  , runSorts "fst . fst . fst . fst"
+      (fst . fst . fst . fst)
+    $ (embedFst . embedFst . embedFst . embedFst) <$> aList
+  , runSorts "fst . fst . fst . fst . fst"
+      (fst . fst . fst . fst . fst)
+    $ (embedFst . embedFst . embedFst . embedFst . embedFst) <$> aList
+  , runSorts "fst . fst . fst . fst . fst . fst"
+      (fst . fst . fst . fst . fst . fst)
+    $ (embedFst . embedFst . embedFst . embedFst . embedFst . embedFst) <$> aList
+  , runSorts "fst . fst . fst . fst . fst . fst . fst"
+      (fst . fst . fst . fst . fst . fst . fst)
+    $ (embedFst . embedFst . embedFst . embedFst . embedFst . embedFst . embedFst) <$> aList
   ]
