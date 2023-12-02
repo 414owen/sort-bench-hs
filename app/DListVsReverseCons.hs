559d72171856d4949e23903701cbd87a6b4b8365
Owen Shepherd <owen@owen.cafe>
2023-12-02 16:17:57 +0100

    feat: Lazy list accumulation

--- app/DListVsReverseCons.hs
+++ app/DListVsReverseCons.hs
@@ -18,8 +18,12 @@ accumulateRev = reverse . foldl' (flip (:)) []
 
 main :: IO ()
 main = defaultMain
-  [ env (pure testList) $ \lst -> bgroup "Appending to lists"
+  [ env (pure testList) $ \lst -> bgroup "All"
     [ bench "reverse . conses" $ nf accumulateRev lst
     , bench "dlist" $ nf accumulateDlist lst
+    ]
+  , env (pure testList) $ \lst -> bgroup "Lazy"
+    [ bench "reverse . conses" $ nf (take 10_000 . accumulateRev) lst
+    , bench "dlist" $ nf (take 10_000 . accumulateDlist) lst
     ]
   ]
